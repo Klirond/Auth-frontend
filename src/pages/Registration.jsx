f49b2c1f90d750997fc4542e3996896cf436cb37
Shaedow2000 <shaedow2000@proton.me>
2026-05-29 19:00:58 +0100

    Create border element

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,10 +1,14 @@
+import Border from "../components/Border";
 import TopSection from "../components/TopSection";
 
 export default function Registration() {
   return (
-    <TopSection
-      title={"Welcome"}
-      text={"Start you journey here by creating a new account."}
-    />
+    <>
+      <TopSection
+        title={"Welcome"}
+        text={"Start you journey here by creating a new account."}
+      />
+      <Border />
+    </>
   );
 }
