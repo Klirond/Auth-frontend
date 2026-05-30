2f9ab40239b91082f72cf48e8450c646a6a447b0
Shaedow2000 <shaedow2000@proton.me>
2026-05-30 17:52:15 +0100

    Create confirmation action hook.

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,9 +1,44 @@
+import { useEffect } from "react";
 import TopSection from "../components/TopSection";
+import { useSearchParams } from "react-router";
+import { useState } from "react";
 
 export default function ConfirmationPage() {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+
+  const [messageTitle, setMessageTitle] = useState("");
+  const [message, setMessage] = useState("");
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(
+    () => async () => {
+      const response = await fetch(`${API_URL}/auth/verify`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          code,
+        }),
+      });
+
+      const json = await response.json();
+
+      setMessageTitle(
+        json.status > 399 && json.status <= 599
+          ? "An error occured"
+          : "You are being redirected",
+      );
+      setMessage(json.message);
+    },
+    [API_URL, code],
+  );
+
   return (
     <>
-      <TopSection title={"Message"} text={"Message from API"} />
+      <TopSection title={messageTitle} text={message} />
     </>
   );
 }
