2497ce137310851d62e85932eff9fcde60644caf
Shaedow2000 <shaedow2000@proton.me>
2026-05-30 09:30:12 +0100

    Fix fonts & Create desktop responsive version of registration page.

--- src/components/SecondaryDesktopFrame.jsx
+++ src/components/SecondaryDesktopFrame.jsx
@@ -5,6 +5,7 @@ export default function SecondaryDesktopFrame() {
         <h1>Already have an account?</h1>
         <p>Quickly login to continue your journey.</p>
       </div>
+      <button className="second-frame-button">Login</button>
     </section>
   );
 }
