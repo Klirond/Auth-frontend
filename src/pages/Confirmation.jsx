108ba30137128c23bf8da9943c5748c49e2b5f07
Shaedow2000 <shaedow2000@proton.me>
2026-06-13 12:35:51 +0100

    Show resend link on confirmation page

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,23 +1,50 @@
 import { useLoaderData, useNavigate } from "react-router";
-import { TopSection } from "klirond-ui-components";
-import { useEffect } from "react";
+import { TopSection, Border, LinkBox } from "klirond-ui-components";
+import { useEffect, useState } from "react";
 
 export default function ConfirmationPage() {
   const navigate = useNavigate();
+  const [error, setError] = useState(false);
   const { status, redirectionPage, messageTitle, messageText } =
     useLoaderData();
 
   useEffect(() => {
     let redirect;
     if (status > 199 && status <= 299) {
-      redirect = setInterval(
-        () => navigate(`/${redirectionPage ? redirectionPage : ""}`),
-        2000,
-      );
+      if (redirectionPage !== "logout-all") {
+        redirect = setInterval(
+          () => navigate(`/${redirectionPage ? redirectionPage : ""}`),
+          2000,
+        );
+      }
+    } else {
+      setError(true);
     }
 
     return () => clearInterval(redirect);
   }, []);
 
-  return <TopSection title={messageTitle} text={messageText} />;
+  return (
+    <>
+      <TopSection title={messageTitle} text={messageText} />
+      {error ? (
+        <>
+          <Border />
+          <LinkBox
+            message={"Click here to "}
+            link={"Try again"}
+            to={
+              redirectionPage === "login"
+                ? "/resend-verification"
+                : redirectionPage === "reset"
+                  ? "/resend-reset"
+                  : "/logout-all-resend"
+            }
+          />
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
